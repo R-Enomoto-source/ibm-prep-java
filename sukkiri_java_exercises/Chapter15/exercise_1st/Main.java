e1aafde0430908920b1f25ce81279e235b4aeba5
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-05 16:59:54 +0900

    ﻿Auto commit: 2026-02-05 16:59:54
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
    
    詳細:
    On branch main Your branch is ahead of 'origin/main' by 12 commits.   (use "git push" to publish your local commits)  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter15/exercise_1st/Main.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
+++ sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
@@ -1,9 +1,11 @@
 package Chapter15.exercise_1st;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Date dt = new Date();
+        Calendar cl = setdt;
     }
 }
