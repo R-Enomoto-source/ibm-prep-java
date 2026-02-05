c01d05383b5a36959fd972d9513cc1ba6e6467b1
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-05 17:18:12 +0900

    ﻿Auto commit: 2026-02-05 17:18:12
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
    
    詳細:
    On branch main Your branch is ahead of 'origin/main' by 31 commits.   (use "git push" to publish your local commits)  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter15/exercise_1st/Main.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
+++ sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
@@ -6,6 +6,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        
+
         // 1. 現在の日時をDate型で取得する。
         Date dt = new Date();
         // 2. 取得した日時情報をCalendarにセットする。
@@ -14,6 +16,7 @@ public class Main {
         // 3. Calendarから「日」の数値を取得する。
         int cld = cl.get(Calendar.DAY_OF_MONTH);
         // 4. 取得した値に100を足した値をCalendarの「日」にセットする。
+        cld += 
         cl.set(Calendar.DAY_OF_MONTH,cld);
         // 5. Calendarの日時情報をDate型に変換する。
         dt = cl.getTime();
