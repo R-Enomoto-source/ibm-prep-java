75c3b81aa655793ba9f1fb2d3e1054e45aa0991d
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-05 17:39:32 +0900

    ﻿Auto commit: 2026-02-05 17:39:31
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter15/exercise_1st/exercise5/Main.java
    
    詳細:
    On branch main Your branch is ahead of 'origin/main' by 50 commits.   (use "git push" to publish your local commits)  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter15/exercise_1st/exercise5/Main.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter15/exercise_1st/exercise5/Main.java
+++ sukkiri_java_exercises/Chapter15/exercise_1st/exercise5/Main.java
@@ -1,12 +1,13 @@
 package Chapter15.exercise_1st.exercise5;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
         LocalDate ld = LocalDate.now();
         LocalDate ldAfter100 = ld.plusDays(100);
-        Date
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern();
         
     }
 }
