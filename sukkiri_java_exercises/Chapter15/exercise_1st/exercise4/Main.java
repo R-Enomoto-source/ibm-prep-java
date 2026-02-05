5265d72ceb9c9846c8de7441b1a0f3012bdb62c9
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-05 17:29:43 +0900

    ﻿Auto commit: 2026-02-05 17:29:43
    変更内容:
    - 削除: 1ファイル
      sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
    
    詳細:
    On branch main Your branch is ahead of 'origin/main' by 41 commits.   (use "git push" to publish your local commits)  Changes not staged for commit:   (use "git add/rm <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	deleted:    sukkiri_java_exercises/Chapter15/exercise_1st/Main.java  Untracked files:   (use "git add <file>..." to include in what will be committed) 	sukkiri_java_exercises/Chapter15/exercise_1st/exercise4/ 	sukkiri_java_exercises/Chapter15/exercise_1st/exercise5/  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter15/exercise_1st/exercise4/Main.java
+++ sukkiri_java_exercises/Chapter15/exercise_1st/exercise4/Main.java
@@ -1,4 +1,4 @@
-package Chapter15.exercise_1st;
+package Chapter15.exercise_1st.exercise4;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
---
+++ sukkiri_java_exercises/Chapter15/exercise_1st/exercise5/Main.java
@@ -0,0 +1,7 @@
+package Chapter15.exercise_1st.exercise5;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
