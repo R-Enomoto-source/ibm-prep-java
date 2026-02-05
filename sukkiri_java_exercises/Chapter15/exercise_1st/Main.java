866b41af2e56b50148483fa43c41f4549ddf4e81
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-05 17:12:24 +0900

    ﻿Auto commit: 2026-02-05 17:12:24
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
    
    詳細:
    On branch main Your branch is ahead of 'origin/main' by 27 commits.   (use "git push" to publish your local commits)  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter15/exercise_1st/Main.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
+++ sukkiri_java_exercises/Chapter15/exercise_1st/Main.java
@@ -15,7 +15,7 @@ public class Main {
         // 4. 取得した値に100を足した値をCalendarの「日」にセットする。
         cl.set(Calendar.DAY_OF_MONTH,cld);
         // 5. Calendarの日時情報をDate型に変換する。
-        Date 
+        Date dt2 = cl.getTime();
         // 6. SimpleDateFormatを用いて、指定された形式でDateインスタンスの内容を表示する。
 
         
