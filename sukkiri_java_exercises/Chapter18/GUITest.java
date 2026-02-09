318c5bbf4de79cab02440941b281c301232e2136
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 18:57:47 +0900

    ﻿Auto commit: 2026-02-09 18:57:42
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter18/GUITest.java
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter18/GUITest.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter18/GUITest.java
+++ sukkiri_java_exercises/Chapter18/GUITest.java
@@ -1,6 +1,5 @@
 package Chapter18;
 
-import java.awt.FlowLayout;
 import javax.swing.*;
 
 public class GUITest {
@@ -8,6 +7,13 @@ public class GUITest {
         JFrame frame = new JFrame("はじめてのGUI");
         JLabel label = new JLabel("Hello World!!");
         JButton button = new JButton("押してね");
+        
+        frame.add(label);
+        frame.add(button);
+        frame.setLayout(new java.awt.FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
     }
 }
 
