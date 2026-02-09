0e311577f213d2d11c4d8b90882883bf8847ab5e
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 11:12:43 +0900

    ﻿Auto commit: 2026-02-09 11:12:38
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter16/first/exercise2/Hero.java
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter16/first/exercise2/Hero.java  Untracked files:   (use "git add <file>..." to include in what will be committed) 	sukkiri_java_exercises/Chapter16/first/exercise2/exercise2_Name  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter16/first/exercise2/Hero.java
+++ sukkiri_java_exercises/Chapter16/first/exercise2/Hero.java
@@ -1,9 +1,8 @@
 package Chapter16.first.exercise2;
 
 public class Hero {
-    public class Hero {
-        private String name;
-        public Hero(String name) { this.name = name; }
-        public String getName() { return this.name; }
-    }
+    private String name;
+    public Hero(String name) { this.name = name; }
+    public String getName() { return this.name; }
 }
+
