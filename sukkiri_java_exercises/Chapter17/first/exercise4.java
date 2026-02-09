a8d19d1968133fa43ef8e9e3240097be93b97d6d
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 18:00:44 +0900

    ﻿Auto commit: 2026-02-09 18:00:39
    変更内容:
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise4.java  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise4.java

--- sukkiri_java_exercises/Chapter17/first/exercise4.java
+++ sukkiri_java_exercises/Chapter17/first/exercise4.java
@@ -1,7 +1,9 @@
 package Chapter17.first;
 
+import java.io.*;
+
 public class exercise4 {
     public static void main(String[] args) {
-        
+        FileReader fr = new FileReader(text.)
     }
 }
