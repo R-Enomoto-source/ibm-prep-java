b7f7dcaffb2ccb2fc1cdf3d874f14e588e35784f
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 11:38:28 +0900

    ﻿Auto commit: 2026-02-09 11:38:17
    変更内容:
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Untracked files:   (use "git add <file>..." to include in what will be committed) 	sukkiri_java_exercises/Chapter16/first/exercise3/  nothing added to commit but untracked files present (use "git add" to track)

---
+++ sukkiri_java_exercises/Chapter16/first/exercise3/Hero.java
@@ -0,0 +1,8 @@
+package Chapter16.first.exercise3;
+
+public class Hero {
+    private String name;
+    public Hero(String name) { this.name = name; }
+    public String getName() { return this.name; }
+}
+
---
+++ sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java
@@ -0,0 +1,19 @@
+package Chapter16.first.exercise3;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class NameDisplay {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("斎藤");
+        Hero hero2 = new Hero("鈴木");
+
+        List<String> heroNames = new ArrayList<String>();
+        heroNames.add(hero1.getName());
+        heroNames.add(hero2.getName());
+        
+        for(String dispay : heroNames){
+            System.out.println(dispay);
+        }
+    }
+}
