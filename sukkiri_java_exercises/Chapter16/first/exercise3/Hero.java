8fe7e2bb4697d283e3f98829def6f6fbfc9baca5
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 11:41:22 +0900

    ﻿Auto commit: 2026-02-09 11:41:17
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter16/first/exercise3/Hero.java
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter16/first/exercise3/Hero.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter16/first/exercise3/Hero.java
+++ sukkiri_java_exercises/Chapter16/first/exercise3/Hero.java
@@ -1,8 +1,15 @@
 package Chapter16.first.exercise3;
 
 public class Hero {
+    
     private String name;
-    public Hero(String name) { this.name = name; }
-    public String getName() { return this.name; }
+    private int 
+
+    public Hero(String name) {
+         this.name = name; 
+        }
+    public String getName() { 
+        return this.name; 
+    }
 }
 
