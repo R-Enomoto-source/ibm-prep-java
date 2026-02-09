fc7d8f8a10ca16bc00c3418a85df9bc572797a65
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 18:04:38 +0900

    ﻿Auto commit: 2026-02-09 18:04:33
    変更内容:
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise4.java  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise4.java

--- sukkiri_java_exercises/Chapter17/first/exercise4.java
+++ sukkiri_java_exercises/Chapter17/first/exercise4.java
@@ -3,9 +3,14 @@ package Chapter17.first;
 import java.io.*;
 
 public class exercise4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         FileReader fr = new FileReader(text.txt);
-        
-
     }
 }
+/*
+実行結果
+Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+        text cannot be resolved to a variable
+
+        at Chapter17.first.exercise4.main(exercise4.java:7) 
+        */
