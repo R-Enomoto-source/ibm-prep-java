8f5736ff4573a5ee647a205dc78eb27a402b521a
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 18:02:06 +0900

    ﻿Auto commit: 2026-02-09 18:02:01
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter17/first/exercise4.java
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise4.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter17/first/exercise4.java
+++ sukkiri_java_exercises/Chapter17/first/exercise4.java
@@ -4,6 +4,8 @@ import java.io.*;
 
 public class exercise4 {
     public static void main(String[] args) {
-        FileReader fr = new FileReader(text.)
+        FileReader fr = new FileReader(text.txt);
+        
+
     }
 }
