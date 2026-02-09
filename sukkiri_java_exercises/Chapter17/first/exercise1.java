f5ded50312c612cc50e62fdbe66a0f6583dcc623
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 14:45:04 +0900

    ﻿Auto commit: 2026-02-09 14:44:59
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter17/first/exercise1.java
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise1.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter17/first/exercise1.java
+++ sukkiri_java_exercises/Chapter17/first/exercise1.java
@@ -1,6 +1,7 @@
 package Chapter17.first;
 
 public class exercise1 {
+    @SuppressWarnings("null")
     public static void main(String[] args) {
         String s = null;
         System.out.println(s.length());
