fe5baf8f6014abb519d2b2fc511f48bebc281e9b
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 11:53:40 +0900

    ﻿Auto commit: 2026-02-09 11:53:34
    変更内容:
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	modified:   sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java

--- sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java
+++ sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java
@@ -1,11 +1,13 @@
 package Chapter16.first.exercise3;
 
+import java.util.Map;
+import java.util.HashMap;
 
 public class NameDisplay {
     public static void main(String[] args) {
         Hero hero1 = new Hero("斎藤",3);
         Hero hero2 = new Hero("鈴木",7);
 
-        Map <String,int> hNameAndNum = new Map<>();
+        Map <String,Intger> hNameAndNum = new HashMap<String,Intger>();
     }
 }
