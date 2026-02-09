16578f76afb3e634cfc245bfc5359f22ed18e24e
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 15:02:28 +0900

    ﻿Auto commit: 2026-02-09 15:02:23
    変更内容:
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise3.java  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise3.java

--- sukkiri_java_exercises/Chapter17/first/exercise3.java
+++ sukkiri_java_exercises/Chapter17/first/exercise3.java
@@ -8,8 +8,9 @@ public class exercise3 {
         parseIntメソッドが発生させうる例外:NumberFormatException
         意味：文字列が解析可能な整数型を含まない場合。
         */     
-    } catch (Exception e) {
-        // TODO: handle exception
+    } catch (NumberFormatException e) {
+        System.out.println("エラー:" + e.getMessage());
+        System.out.println("");
     }
     }
 }
