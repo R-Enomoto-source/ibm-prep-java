0f90e57ade35036f2141062b2f2fdcd52c26091f
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 11:15:59 +0900

    ﻿Auto commit: 2026-02-09 11:15:54
    変更内容:
    - 削除: 1ファイル
      sukkiri_java_exercises/Chapter16/first/exercise2/exercise2_Name
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	new file:   sukkiri_java_exercises/Chapter16/first/exercise2/NameDisplay.java 	deleted:    sukkiri_java_exercises/Chapter16/first/exercise2/exercise2_Name  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter16/first/exercise2/NameDisplay.java

---
+++ sukkiri_java_exercises/Chapter16/first/exercise2/NameDisplay.java
@@ -0,0 +1,10 @@
+package Chapter16.first.exercise2;
+
+public class NameDisplay {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("斎藤");
+        Hero hero2 = new Hero("鈴木");
+
+        
+    }
+}
