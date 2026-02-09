a51a75c7536bf0afcd4f8ddf55f71f476166d25e
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 11:56:54 +0900

    ﻿Auto commit: 2026-02-09 11:56:49
    変更内容:
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	modified:   sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java

--- sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java
+++ sukkiri_java_exercises/Chapter16/first/exercise3/NameDisplay.java
@@ -1,13 +1,20 @@
 package Chapter16.first.exercise3;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 public class NameDisplay {
     public static void main(String[] args) {
+        //インスタンス生成
         Hero hero1 = new Hero("斎藤",3);
         Hero hero2 = new Hero("鈴木",7);
 
-        Map <String,Intger> hNameAndNum = new HashMap<String,Intger>();
+        //Mapの作成と格納
+        Map <String,Integer> hNameAndNum = new HashMap<String,Integer>();
+        hNameAndNum.put(hero1.getName(),hero1.getKilleEnemyNum());
+        hNameAndNum.put(hero2.getName(),hero2.getKilleEnemyNum());
+
+        //Mapの取り出しと表示
+        
+
     }
 }
