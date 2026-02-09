c8d6b8d9bc4185e04e55cabad0fe10353c8b6db8
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 18:56:26 +0900

    ﻿Auto commit: 2026-02-09 18:56:20
    変更内容:
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Untracked files:   (use "git add <file>..." to include in what will be committed) 	sukkiri_java_exercises/Chapter18/  nothing added to commit but untracked files present (use "git add" to track)

---
+++ sukkiri_java_exercises/Chapter18/GUITest.java
@@ -0,0 +1,13 @@
+package Chapter18;
+
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+public class GUITest {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("はじめてのGUI");
+        JLabel label = new JLabel("Hello World!!");
+        JButton button = new JButton("押してね");
+    }
+}
+
