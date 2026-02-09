08da003a1ee9f988c2d54d72feeefb5a32b0b421
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 14:46:30 +0900

    ﻿Auto commit: 2026-02-09 14:46:25
    変更内容:
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Untracked files:   (use "git add <file>..." to include in what will be committed) 	sukkiri_java_exercises/Chapter17/first/exercise2.java  nothing added to commit but untracked files present (use "git add" to track)

---
+++ sukkiri_java_exercises/Chapter17/first/exercise2.java
@@ -0,0 +1,12 @@
+package Chapter17.first;
+
+public class exercise2 {
+    public static void main(String[] args) {
+        String s = null;
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }System.out.println(s.length());
+    }
+}
