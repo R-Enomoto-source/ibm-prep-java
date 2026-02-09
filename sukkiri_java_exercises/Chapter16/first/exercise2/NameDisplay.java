6d590ff96691af2f493e718ae4b03ef72ddb14de
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 11:19:12 +0900

    ﻿Auto commit: 2026-02-09 11:19:07
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter16/first/exercise2/NameDisplay.java
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter16/first/exercise2/NameDisplay.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter16/first/exercise2/NameDisplay.java
+++ sukkiri_java_exercises/Chapter16/first/exercise2/NameDisplay.java
@@ -1,10 +1,12 @@
 package Chapter16.first.exercise2;
 
+import java.util.ArrayList;
+
 public class NameDisplay {
     public static void main(String[] args) {
         Hero hero1 = new Hero("斎藤");
         Hero hero2 = new Hero("鈴木");
 
-        List<Hero> heroNames = 
+        List<Hero> heroNames = new ArrayList<>();
     }
 }
