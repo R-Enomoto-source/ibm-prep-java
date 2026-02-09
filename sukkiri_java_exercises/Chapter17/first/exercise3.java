339119ab09012dcc19695a1a21e30cbe489f21e5
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-09 15:03:50 +0900

    ﻿Auto commit: 2026-02-09 15:03:45
    変更内容:
    - 変更: 1ファイル
      sukkiri_java_exercises/Chapter17/first/exercise3.java
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   sukkiri_java_exercises/Chapter17/first/exercise3.java  no changes added to commit (use "git add" and/or "git commit -a")

--- sukkiri_java_exercises/Chapter17/first/exercise3.java
+++ sukkiri_java_exercises/Chapter17/first/exercise3.java
@@ -1,6 +1,7 @@
 package Chapter17.first;
 
 public class exercise3 {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
     try {
         int i = Integer.parseInt("三");
@@ -10,7 +11,7 @@ public class exercise3 {
         */     
     } catch (NumberFormatException e) {
         System.out.println("エラー:" + e.getMessage());
-        System.out.println("");
+        System.out.println("文字列には変換可能な数値を入力してください");
     }
     }
 }
