7053b8fe43dac7e4e68adc306ebe0488d1ce6795
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-10 11:05:50 +0900

    ﻿Auto commit: 2026-02-10 11:05:45
    変更内容:
    - 削除: 7ファイル
      java_blackbook/Chapter2_1_once, java_blackbook/Chapter2_2_once, java_blackbook/Chapter2_3_once, java_blackbook/chapter1_6.java, java_blackbook/chapter1_8.java, java_blackbook/chapter1_9.java, java_blackbook/chapter1_once
    
    詳細:
    On branch main Your branch is up to date with 'origin/main'.  Changes not staged for commit:   (use "git add/rm <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	deleted:    java_blackbook/Chapter2_1_once 	deleted:    java_blackbook/Chapter2_2_once 	deleted:    java_blackbook/Chapter2_3_once 	deleted:    java_blackbook/chapter1_6.java 	deleted:    java_blackbook/chapter1_8.java 	deleted:    java_blackbook/chapter1_9.java 	deleted:    java_blackbook/chapter1_once  Untracked files:   (use "git add <file>..." to include in what will be committed) 	java_blackbook/Chapter1/ 	java_blackbook/Chapter2/ 	learningNote/LearningNote_20260210.md  no changes added to commit (use "git add" and/or "git commit -a")

--- java_blackbook/Chapter1/Chapter1_8.java
+++ java_blackbook/Chapter1/Chapter1_8.java
@@ -1,3 +1,4 @@
+package Chapter1;
 public class Chapter1_8 {
     public static void main(String[] args) {
         System.out.println(args.length);
--- java_blackbook/Chapter1/Chapter1_9.java
+++ java_blackbook/Chapter1/Chapter1_9.java
@@ -1,3 +1,4 @@
+package Chapter1;
 public class Chapter1_9 {
     public static void main(String[] args) {
     System.out.println("sample");
